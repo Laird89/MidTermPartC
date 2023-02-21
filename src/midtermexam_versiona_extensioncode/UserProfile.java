d5c0a952576899fcbbc7d59c178c35b5d2fa31a0
Laird89 <117320855+Laird89@users.noreply.github.com>
2023-02-21 10:20:16 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -3,6 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*
+* Adding a comment here to test out fetching and merging
+*/
 package midtermexam_versiona_extensioncode;
 
 /**
